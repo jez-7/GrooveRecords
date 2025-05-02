7c8ebdac6d6f431e867da9db4f9dbdc3ed43465c
jez-7 <juanemanuelzamora@gmail.com>
2025-05-01 23:59:30 -0300

    arreglando CORS

--- src/main/java/config/CorsConfig.java
+++ src/main/java/config/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig {
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
 	                registry.addMapping("/**")
-	                        .allowedOriginPatterns("http://localhost:8080") 
+	                        .allowedOriginPatterns("http://localhost:8080",
+									               "https://groove-records-nag2.onrender.com"
+							)
 	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	                        .allowedHeaders("Authorization", "Content-Type", "*")
 	                        .exposedHeaders("Authorization");
